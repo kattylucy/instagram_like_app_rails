ada4ccbd5edd7d4f435901819b1c17b2f709c2d6
kattylucy <kattybarroso1321@gmail.com>
2021-01-09 16:15:45 -0800

    created post form. Images not working yet

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
-  resources :posts, only: [:index, :show, :create]
-  resources :photos, only: [:create]
+  resources :posts, only: [:index, :show, :create] do 
+    resources :photos, only: [:create]
+  end
 end
